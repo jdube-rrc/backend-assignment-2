1655175cc2536e29d8f2a91b63792aec4def2241
Jared Dube <jdube3@academic.rrc.ca>
2025-09-27 18:03:26 -0500

    Updated imports to use new routes and error handling middleware, and implemented them into the application runtime.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import branchRoutes from "./api/v1/routes/branchRoutes";
+import employeeRoutes from "./api/v1/routes/employeeRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
+
+app.use("/api/v1/branches", branchRoutes);
+app.use("/api/v1/employees", employeeRoutes);
 
 app.get("/api/v1/health", (req, res) => {
   res.json({
@@ -14,5 +20,7 @@ app.get("/api/v1/health", (req, res) => {
     version: "1.0.0",
   });
 });
+
+app.use(errorHandler);
 
 export default app;
