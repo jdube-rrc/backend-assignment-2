98cfed130e7723fe8869932c8b7ad7636db704ba
Jared Dube <jdube3@academic.rrc.ca>
2025-10-18 22:58:42 -0500

    Updated to use database private key, not sure how this changed

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,8 +1,8 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-// You'll n eed to replace this with your actual service account file name
-import serviceAccount from "back-end-module-3-dcb5d-firebase-adminsdk-fbsvc-60b3c04656.json";
+// You'll need to replace this with your actual service account file name
+import serviceAccount from "../back-end-module-3-dcb5d-firebase-adminsdk-fbsvc-60b3c04656.json";
 
 // initialize the Firebase app with our service account key
 initializeApp({
